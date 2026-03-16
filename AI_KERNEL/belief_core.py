cdb63cb17835d0e7e6192986c3b8cd907af3ad65
Nithikorn Bunsrang <varjayd01@gmail.com>
2026-03-16 09:05:36 +0700

    Create belief_core.py

---
+++ AI_KERNEL/belief_core.py
@@ -0,0 +1,18 @@
+BELIEF_CORE = {
+
+"choice_gt_zero":True,
+
+"survival_floor_first":True,
+
+"emotion_is_signal":True,
+
+"truth_platform_independent":True,
+
+"nature_alignment":True
+
+}
+
+
+def enforce_belief():
+
+    return BELIEF_CORE
